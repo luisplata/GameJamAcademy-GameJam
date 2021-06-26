6a0f25bb7576872a4fb19d320c81e18872812ead
Sirix <sirixb@gmail.com>
2021-06-26 17:23:33 -0500

    Dance characters

--- Assets/Animations.cs
+++ Assets/Animations.cs
@@ -21,11 +21,14 @@ public class Animations : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
             Give();
 
+        if (Input.GetKeyDown(KeyCode.D))
+            Dance();
+
     }
 
     private void Move()
     {
-            animator.SetFloat("speed",input.magnitude);      
+        animator.SetFloat("speed",input.magnitude);      
     }
     private void Power()
     {
@@ -34,6 +37,10 @@ public class Animations : MonoBehaviour
     private void Give()
     {
         animator.SetTrigger("give");
+    }
+    private void Dance()
+    {
+        animator.SetTrigger("dance");
     }
 
 }
