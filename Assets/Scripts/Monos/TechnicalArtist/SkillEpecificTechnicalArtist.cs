cb0ade7e499f074f3682eddeccf831776c76f8ab
Sirix <sirixb@gmail.com>
2021-06-25 23:53:40 -0500

    Models

---
+++ Assets/Scripts/Monos/TechnicalArtist/SkillEpecificTechnicalArtist.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillEpecificTechnicalArtist : Skill
+{
+    public override bool HasPushSkill()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ActionSkill()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
