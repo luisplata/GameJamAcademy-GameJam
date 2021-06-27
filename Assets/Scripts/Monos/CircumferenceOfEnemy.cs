ffae004199f1b71cb08ac561cd85d9de2e8bb524
Luis Plata <www.luisplata@gmail.com>
2021-06-27 01:05:43 -0500

    adding RB for the point

--- Assets/Scripts/Monos/CircumferenceOfEnemy.cs
+++ Assets/Scripts/Monos/CircumferenceOfEnemy.cs
@@ -5,6 +5,7 @@ public class CircumferenceOfEnemy : MonoBehaviour, ICircumferenceOfEnemy
     [SerializeField] private GameObject pointToObject;
     [SerializeField] private GameObject targetToOpponents;
     [SerializeField] private int multiply;
+    [SerializeField] private Rigidbody rb;
     private IEmployee _employee;
     private Vector3 randomCircle;
 
@@ -18,11 +19,18 @@ public class CircumferenceOfEnemy : MonoBehaviour, ICircumferenceOfEnemy
     {
         return targetToOpponents.transform.position + randomCircle;
     }
-
+    private void Rotating (float horizontal, float vertical)
+    {
+        Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        Quaternion newRotation = Quaternion.Lerp(rb.rotation, targetRotation, 100 * Time.deltaTime);
+        rb.MoveRotation(newRotation);
+    }
     public void Rotate()
     {
         var targetDir = _employee.GetObjective() - _employee.GetPosition();
-        var angle = Vector3.Angle(targetDir, -transform.forward);
-        pointToObject.transform.eulerAngles = new Vector3(0, angle, 0);
+        /*var angle = Vector3.Angle(targetDir, -transform.forward);
+        pointToObject.transform.eulerAngles = new Vector3(0, angle, 0);*/
+        Rotating(targetDir.x, targetDir.z);
     }
 }
